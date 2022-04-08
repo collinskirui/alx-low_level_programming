34583f83f4c72936565c8bef49157eba2c4a334b
collinskirui <collins.kirui@strathmore.edu>
2022-04-08 10:39:47 -0700

    My commit message

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -9,9 +9,11 @@ int main(void)
 {
 char alphabet;
 
-for (alphabet = 122 ; alphabet > = 97 ; alphabet--)
+for (alphabet = 122 ; alphabet >= 97 ; alphabet--)
 {
+
 putchar(alphabet);
+
 }
 
 putchar ('\n');
