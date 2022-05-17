f35d3e010dde558365e665bbe3e336642c659a72
collinskirui <collins.kirui@strathmore.edu>
2022-05-17 05:42:10 -0700

    My commit message

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,9 +1,10 @@
+
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
 
 /**
- * print_numbers - function that prints numbers, followed by a new line
+ * print_strings - function that prints strings, followed by a new line
  * @separator: pointer to the separator
  * @n: const unsigned int passed from main,
  * count argument indicating the number
@@ -12,15 +13,17 @@
  * Return: void
  */
 
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	char *tmp;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%i", va_arg(args, int));
+		tmp = va_arg(args, char *);
+		printf("%s", tmp != NULL ? tmp : "(nil)");
 		if (i != n - 1)
 			if (separator)
 				printf("%s", separator);
